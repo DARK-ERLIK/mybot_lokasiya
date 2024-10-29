621067e0f03cd8efbe4ab8ec9705e75c6876b5f2
Ravshan ISHIMOV <remcorps2@gmail.com>
2024-10-29 14:11:57 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,24 +1,23 @@
+import sqlite3
+from datetime import datetime
+connection = sqlite3.connect("mybot.db")
+
 # Функция для инициализации базы данных и создания таблицы пользователей
 def init_db():
     connection = sqlite3.connect("mybot.db")
     sql = connection.cursor()
-
-    # Создаем таблицу пользователей, если она еще не создана
-    sql.execute('''CREATE TABLE IF NOT EXISTS users (
-                    user_id INTEGER PRIMARY KEY,
-                    name TEXT,
-                    phone_number TEXT,
-                    address TEXT,
-                    reg_date DATETIME)''')
+    sql.execute("CREATE TABLE IF NOT EXISTS users "
+        "(user_id INTEGER PRIMARY KEY, name TEXT, "
+        "phone_number TEXT, address TEXT, "
+        "reg_date DATETIME, language TEXT);")
     connection.commit()
     connection.close()
 
-
-# Функция для добавления пользователя
-def add_user(user_id, name, phone_number, address):
-    connection = sqlite3.connect("bot_database.db")
+# Функция для добавления пользователя в базу данных
+def add_user(user_id, name, phone_number, address, language):
+    connection = sqlite3.connect("mybot.db")
     sql = connection.cursor()
-    sql.execute("INSERT INTO users (user_id, name, phone_number, address, reg_date) VALUES (?, ?, ?, ?, ?)",
-                (user_id, name, phone_number, address, datetime.now()))
+    sql.execute("INSERT INTO users (user_id, name, phone_number, address, reg_date, language) VALUES (?, ?, ?, ?, ?, ?)",
+                (user_id, name, phone_number, address, datetime.now(), language))
     connection.commit()
     connection.close()
